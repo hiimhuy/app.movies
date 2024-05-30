7f4d98161643ee35c234812de292858a5b961b43
Cong Huy <conghuy123zzz@gmail.com>
2024-05-30 17:25:00 +0700

    fix build SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://https://ofilm.vercel.app/.com',
+  siteUrl: 'https://https://ofilm.vercel.app/.com',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
